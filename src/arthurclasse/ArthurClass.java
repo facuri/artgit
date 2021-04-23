490f5a727816d8c49792bca6efe35c92aa257614
dalmo <mffacuri@terra.com.br>
2021-04-23 20:50:07 -0300

    exemplo com laço repeticão for

--- src/arthurclasse/ArthurClass.java
+++ src/arthurclasse/ArthurClass.java
@@ -4,7 +4,15 @@ public class ArthurClass {
 
 	public static void main(String[] args) {
 		 
+		int i, som;
+		som = 0;
 		//exemplo for
+		for(i = 1; i <= 5; i++){
+			
+			System.out.println(som += i);
+			
+		}
+		
 
 	}
 
